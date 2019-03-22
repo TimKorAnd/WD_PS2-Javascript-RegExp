b9cc1f10b546231d95cf6bb3c9ae758e00420510
timkor <timkorand@gmail.com>
2019-03-22 23:45:09 +0200

    1,2 - complete;  2_decomposition

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* CONSTANTS */
 const NUMBER_VALID_REGEX = /^-?\d+$/;   //for task_1 regexp
 const TIME_IN_SECOND_VALID_REGEX = /^\d+$/;   //for task_2 seconds regexp
-//const TIME_IN_HMS_VALID_REGEX = /^\d\d:\d\d:\d\d+$/;   //for task_2 HMS regexp
 const TIME_IN_HMS_VALID_REGEX = /^((0(?=\d)|1(?=\d)|2(?=[0-4]))\d):([0-5](?=\d)\d):([0-5](?=\d)\d)$/;   //for task_2 HMS regexp
 
 const SECONDS_IN_HMS = [60 * 60, 60, 1 ];
@@ -45,8 +44,10 @@ function getSum(regTemplate, btnId, ...inputsId) {
     resultOutput.innerText = sumSpecNum(numberFirst, numberSecond);
 }
 /*validation
+* return true if all inputs valid, false if not;
 * btnId - btn for activate/deactivate
-* regTemplate - reg.*/
+* regTemplate - reg.expression for test inputs
+* inputsId one or some inputs*/
 function validation(btnId, regTemplate, inputsId){
 
     let isAllInputsValid = true;
@@ -70,12 +71,12 @@ function validation(btnId, regTemplate, inputsId){
         }
     }
 
-
     document.getElementById(btnId).disabled = !isAllInputsValid;
     return isAllInputsValid;
 }
 
-function transferFromSecond(regTime, outputId, inputId){
+/*output in hms*/
+function outputInHMS(regTime, outputId, inputId){
     let timeInSecond = document.getElementById(inputId).value;
     const resultTimeTransform = document.getElementById(outputId);
     if (!validation('time-form__btn', regTime, [inputId])){
@@ -83,50 +84,53 @@ function transferFromSecond(regTime, outputId, inputId){
         resultTimeTransform.className = 'sum-form__input--invalid';
         return;
     };
+    /*transfer from second to hms*/
+    function transferFromSecond() {
+        let arrayHMS = SECONDS_IN_HMS.map((item) => {
+            let tis = timeInSecond;
+            timeInSecond %= item;
+            return Math.floor(tis / item);
+        });
 
-    let arrayHMS  = SECONDS_IN_HMS.map((item) => {
-        let tis = timeInSecond;
-        timeInSecond %= item;
-        return Math.floor(tis / item);
+        arrayHMS.forEach((x, j) => {
+            arrayHMS[j] = (x.toString().padStart(2, '0'));
+        });
 
-    });
+        resultTimeTransform.value = arrayHMS.join(':');
+        resultTimeTransform.className = 'sum-form__input--valid';
+    }
 
-    arrayHMS.forEach((x, j) => {
-        arrayHMS[j] = (x.toString().padStart(2,'0'));
-    });
-
-    resultTimeTransform.value = arrayHMS.join(':');
-    //validation('time-form__btn', TIME_IN_HMS_VALID_REGEX, [outputId]);
-    resultTimeTransform.className = 'sum-form__input--valid';
+    transferFromSecond();
 
 }
 
-
-function transferFromHMS(regHMS, outputId, inputId){
+/*output seconds*/
+function outputInSeconds(regHMS, outputId, inputId){
 
     const timeInSecond = document.getElementById(inputId).value;
     const resultTimeTransform = document.getElementById(outputId);
     if (!validation('time-form__btn', regHMS, [inputId])){
-        resultTimeTransform.placeholder =  'enter correct hh:mm:ss';
+        resultTimeTransform.placeholder = 'enter correct hh:mm:ss';
         resultTimeTransform.className = 'sum-form__input--invalid';
         return;
     };
+    /*transfer time from HMS to seconds*/
+    function transferFromHMS() {
+        let seconds = timeInSecond.split(':').reduce((prevVal, currVal, currIndex, seconds) => {
+            return prevVal + currVal * SECONDS_IN_HMS[currIndex];
+        }, 0);
+        return seconds;
+    }
 
-    let seconds = timeInSecond.split(':').reduce((prevVal, currVal, currIndex, seconds) =>{
-        return prevVal + currVal * SECONDS_IN_HMS[currIndex];
-    }, 0);
+    let seconds = transferFromHMS();
+
     resultTimeTransform.value =  seconds.toString();
-    //validation('time-form__btn', TIME_IN_SECOND_VALID_REGEX, [outputId]);
     resultTimeTransform.className = 'sum-form__input--valid';
 
 }
-
+/*return true if enter pressed*/
 function enterPressed(event) {
-    if (event.which == 13 || event.keyCode == 13) {
-
-        return true;
-    }
-    return false;
+    return (event.which == 13 || event.keyCode == 13);
 }
 
 function test(){
